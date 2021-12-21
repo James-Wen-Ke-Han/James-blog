1d07c7222484fbb4408e24baa8101839d223fd7e
James-Wen-Ke-Han <94221252+James-Wen-Ke-Han@users.noreply.github.com>
2021-12-21 21:35:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -220,6 +220,7 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+#main func
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
