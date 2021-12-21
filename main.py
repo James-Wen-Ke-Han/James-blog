f10be04ff875543ea520f5c2b7378b6f6989612c
James <wen.kehan@gmail.com>
2021-12-21 21:38:36 +0800

    update main

--- main.py
+++ main.py
@@ -220,7 +220,6 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-#main func
-
+#main func, use non default port
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
