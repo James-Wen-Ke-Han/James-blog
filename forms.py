e603217326ab457cfd27336a4c96802eefc4456f
James <wen.kehan@gmail.com>
2021-12-21 22:23:22 +0800

    update imports

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.fields.simple import EmailField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, URL, Email, Length
 from flask_ckeditor import CKEditorField
 
